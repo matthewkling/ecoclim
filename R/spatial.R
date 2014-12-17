c9f3c4962bd304a7fcdccc3a80b9a3e0492248ba
Matthew Kling <mattkling@gmail.com>
2014-12-17 11:44:25 -0800

    partitionRaster function

--- R/spatial.R
+++ R/spatial.R
@@ -38,6 +38,7 @@ clp <- function(raster, polygon){
       mask(raster, polygon)
 }
 
+#######################
 
 #' Wrapper around loading and clipping by a shapefile.
 #'
@@ -50,6 +51,28 @@ clp <- function(raster, polygon){
 boundaryCrop <- function(raster, boundary.directory, boundary.filename) {
       boundary <- getBoundary(boundary.directory, boundary.filename)
       crp(raster, boundary)
+}
+
+
+#######################
+
+#' Split a Raster* object into strips.
+#'
+#' Partition a raster into vertical strips. Useful for parallel processing.
+#'
+#' @param raster A *Raster object to be partitioned.
+#' @param nchunks Integer representing number of partitions.
+#' @param chunk Integer representing which of the paritions to create.
+#' @param subextent Optional extent object indicating a subdomain of the raster
+#'   that should be partitioned.
+#' @return A cropped strip of the input raster object.
+partitionRaster <- function(raster, nchunks, chunk, subextent=NA){
+      e <- extent(raster)
+      if(!is.na(subextent)) e <- subextent
+      x <- seq(e@xmin, e@xmax, (e@xmax-e@xmin)/(nchunks))
+      e@xmin <- x[chunk] - 2 * res(raster)[1]
+      e@xmax <- x[chunk+1]  + 2 * res(raster)[1]
+      return(trim(crop(raster, e)))
 }
 
 
